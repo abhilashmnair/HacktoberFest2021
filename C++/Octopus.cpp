5ac2f5717f388e79e6cbefe60e03cb52df7e32d0
Abhilash M Nair <abhilashmnair20@gmail.com>
2021-10-03 20:55:54 +0530

    Added Dr.Octopus

--- C++/Octopus.cpp
+++ C++/Octopus.cpp
@@ -1,16 +1,10 @@
-// Highly Complicated C++ program 
-
 // Header file for input output functions
 #include<iostream>
-
 using namespace std;
 
 // main function -
 // where the execution of program begins
 int main()
-{
-	
-	cout<<"Hello Peter...!!!";
-	
+{	cout<<"Hello Peter!!";	// Print to output
 	return 0;
 }
