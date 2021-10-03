650b8f01f8e969ef090170f76b61536275fe0c21
Abhishek T <71754819+abhi-shek421@users.noreply.github.com>
2021-10-03 20:51:25 +0530

    Create Octopus.cpp

---
+++ C++/Octopus.cpp
@@ -0,0 +1,16 @@
+// Highly Complicated C++ program 
+
+// Header file for input output functions
+#include<iostream>
+
+using namespace std;
+
+// main function -
+// where the execution of program begins
+int main()
+{
+	
+	cout<<"Hello Peter...!!!";
+	
+	return 0;
+}
