4e5e5b35cfd386a8f3ed15b4b86d093180ba503e
subho004 <91646273+subho004@users.noreply.github.com>
2021-10-04 08:48:23 +0530

    palindrone.cpp
    c++ program to detect a palindrone number

---
+++ palindrone.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+//this is a program to check whether a number is palindrone
+// A palindromic number is a number that remains the same when its digits are reversed
+// 16461 is a palindrone number
+//contributed by subho004
+
+int main()
+{
+     int n, num, digit, rev = 0;
+
+     cout << "Enter a positive number: ";
+     cin >> num;
+
+     n = num;
+
+     do
+     {
+         digit = num % 10;
+         rev = (rev * 10) + digit;
+         num = num / 10;
+     } while (num != 0);
+
+     cout << " The reverse of the number is: " << rev << endl;
+
+     if (n == rev)
+         cout << " The number is a palindrome.";
+     else
+         cout << " The number is not a palindrome.";
+
+    return 0;
+}
