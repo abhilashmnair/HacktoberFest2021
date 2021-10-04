3211e778e7466f7bb73088c8a2062dd3ffb40179
Chirag Talreja <90869160+ct-27@users.noreply.github.com>
2021-10-04 13:34:46 +0530

    Update Binary_tree_is_symmetirc_or_not.cpp

--- C++/Binary_tree_is_symmetirc_or_not.cpp
+++ C++/Binary_tree_is_symmetirc_or_not.cpp
@@ -1,27 +1,118 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    bool isIdentical(TreeNode *r1, TreeNode *r2)
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+ // } Driver Code Ends
+/*
+Structure of the node of the tree is as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution{
+    public:
+    // return true/false denoting whether the tree is Symmetric or not
+    bool isIdentical(Node *r1, Node *r2)
     {
         if(!r1&&!r2)
         return true;
         if(!r1||!r2)
         return false;
-        if(r1->val!=r2->val)
+        if(r1->data!=r2->data)
         return false;
         return isIdentical(r1->left,r2->right)&&isIdentical(r1->right,r2->left);
     }
-    bool isSymmetric(TreeNode* root) 
+    bool isSymmetric(struct Node* root)
     {
         if(!root)
         return true;
@@ -29,3 +120,33 @@ public:
 	    return b;
     }
 };
+
+// { Driver Code Starts.
+
+/* Driver program to test size function*/
+
+  
+
+int main() {
+
+   
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        
+        Node* root = buildTree(s);
+
+        vector<int> ans;
+        Solution ob;
+        if(ob.isSymmetric(root)){
+            cout<<"True"<<endl;
+        }
+        else{ 
+            cout<<"False"<<endl;
+        }
+    }
+    return 0;
+}
+  // } Driver Code Ends
