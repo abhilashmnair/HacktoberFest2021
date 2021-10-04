a03b0d73fc4de4613254a926db0a8d65672265ef
Ananthakrishnan Nair RS <61831021+akrish4@users.noreply.github.com>
2021-10-04 21:59:14 +0530

    added code

--- Python/insertion_sort.py
+++ Python/insertion_sort.py
@@ -6,9 +6,7 @@ def insertionSort(arr):
   
         key = arr[i]
   
-        # Move elements of arr[0..i-1], that are
-        # greater than key, to one position ahead
-        # of their current position
+       
         j = i-1
         while j >=0 and key < arr[j] :
                 arr[j+1] = arr[j]
