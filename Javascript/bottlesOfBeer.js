c8fae90ca887b5082a7c97772fc30e6d3dd3d155
Cam <camcat724@hotmail.com>
2021-10-04 22:26:27 -0500

    Added a while loop that counts down bottles of beer
     Here a a while loop for an old classic song that counts down from 99 bottles of beer to 1. Thanks enjoy.

---
+++ Javascript/bottlesOfBeer.js
@@ -0,0 +1,17 @@
+
+// Here a a while loop for an old classic song that counts down from 99 bottles of beer to 1. Thanks enjoy.
+
+
+
+var numberOfBottles = 99
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    } 
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
