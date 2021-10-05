a809e87a6ea28227dc6a815a06574ff99116e554
Omkar Arora <57015056+omkarxx@users.noreply.github.com>
2021-10-05 15:44:00 +0530

    Create SortArray.c

---
+++ SortArray.c
@@ -0,0 +1,42 @@
+/*
+    * C program to accept a set of numbers and arrange them
+    * in a descending order
+    */
+ 
+    #include <stdio.h>
+    void main ()
+    {
+ 
+        int number[30];
+ 
+        int i, j, a, n;
+        printf("Enter the value of N\n");
+        scanf("%d", &n);
+ 
+        printf("Enter the numbers \n");
+        for (i = 0; i < n; ++i)
+	        scanf("%d", &number[i]);
+ 
+        /*  sorting begins ... */
+ 
+        for (i = 0; i < n; ++i) 
+        {
+            for (j = i + 1; j < n; ++j) 
+            {
+                if (number[i] < number[j]) 
+                {
+                    a = number[i];
+                    number[i] = number[j];
+                    number[j] = a;
+                }
+            }
+        }
+ 
+        printf("The numbers arranged in descending order are given below\n");
+ 
+        for (i = 0; i < n; ++i) 
+        {
+            printf("%d\n", number[i]);
+        }
+ 
+    }
