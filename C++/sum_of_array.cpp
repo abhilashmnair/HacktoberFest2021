111635ebd0900625387d32b1bf88f2d977b14558
Shivam Chopra <60386222+imshivam18@users.noreply.github.com>
2021-10-05 18:28:10 +0530

    sum_of_array.cpp
    How to find sum of an array.
    Hope you Like it Sir.

---
+++ C++/sum_of_array.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+		int n, sum=0;
+
+		cin >> n;
+
+		 int arr[n];
+
+		 for( int i=0 ; i < n ; i++ )
+	{
+  		
+      cin >> arr[i];
+
+       	cout << arr[i] <<" " ;
+
+    	sum += arr[i];
+
+    }  	
+    	cout << endl ;
+
+    	cout << sum << endl ;
+
+	return 0;
+}	
