ae7506ae9f54a88b6f4570b13a7c1b1c4b2efa1b
Amit giri <77196415+Amitlpu-840@users.noreply.github.com>
2021-10-06 14:50:41 +0530

    added code for body mass index
    added the code for body mass index in the python language. Please do let me know if any conflict or issue is there.

---
+++ Python/BMI.py
@@ -0,0 +1,25 @@
+#code to find the body mass index by Amitlpu-840
+height = float(input("Enter height in meters: "))
+weight = float(input("Enter weight in kg: "))
+
+
+bmi = weight / (height ** 2)
+
+
+print("Your BMI is: {0} and you are: ".format(bmi), end="")
+
+# conditions
+if bmi < 16:
+    print("severely underweight")
+
+elif bmi >= 16 and bmi < 18.5:
+    print("underweight")
+
+elif bmi >= 18.5 and bmi < 25:
+    print("Healthy")
+
+elif bmi >= 25 and bmi < 30:
+    print("overweight")
+
+elif bmi >= 30:
+    print("severely overweight")
