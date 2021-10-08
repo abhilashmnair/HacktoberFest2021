6152ba608b9dd4c5fd09e3b5fa1c192da8dd422a
sljtheultima <sljtheultima@users.noreply.github.com>
2021-10-08 10:03:48 +0800

    Create QuickSort.py

---
+++ Python/QuickSort.py
@@ -0,0 +1,31 @@
+def partition(nums, low, high):
+    pivot = nums[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while nums[i] < pivot:
+            i += 1
+
+        j -= 1
+        while nums[j] > pivot:
+            j -= 1
+
+        if i >= j:
+            return j
+        nums[i], nums[j] = nums[j], nums[i]
+
+
+def quick_sort(nums):
+    def _quick_sort(items, low, high):
+        if low < high:
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
+
+
+random_list_of_nums = [25, 10, 34, 16, 99]
+quick_sort(random_list_of_nums)
+print(random_list_of_nums)
