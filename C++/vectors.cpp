c466eb2feeab05aca5feff827c35680d933fcc43
Sentrydeca <63970490+Sentrydeca@users.noreply.github.com>
2021-10-16 17:28:29 +0530

    Basic vector iterators in C++4

---
+++ C++/vectors.cpp
@@ -0,0 +1,39 @@
+// C++ Program illustrating 
+// Basic vector iterators
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    // Declaration of vector
+	vector<int> V;
+
+    // Taking input
+	for (int i = 1; i <= 5; i++)
+		V.push_back(i);
+
+    // Printing
+	cout << "Output of begin and end: ";
+	for (auto i = V.begin(); i != V.end(); ++i)
+		cout << *i << " ";
+
+    // Printing
+	cout << "\nOutput of cbegin and cend: ";
+	for (auto i = V.cbegin(); i != V.cend(); ++i)
+		cout << *i << " ";
+
+    // Printing in reverse order
+	cout << "\nOutput of rbegin and rend: ";
+	for (auto ir = V.rbegin(); ir != V.rend(); ++ir)
+		cout << *ir << " ";
+
+    // Printing in reverse order
+	cout << "\nOutput of crbegin and crend : ";
+	for (auto ir = V.crbegin(); ir != V.crend(); ++ir)
+		cout << *ir << " ";
+
+	return 0;
+}
